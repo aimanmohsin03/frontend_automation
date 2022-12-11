a2c457bb34bcd897b43dd83dbb6d801a21d77bec
aimanmohsin03 <99633632+aimanmohsin03@users.noreply.github.com>
2022-12-11 15:56:33 +0500

    Update index.py

--- index.py
+++ index.py
@@ -7,22 +7,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from tkinter import Tk
+import time
 
 PATH = '/Users/aimanmohsin/Downloads/chromedriver'
 SERVICE = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=SERVICE)
 driver.maximize_window()
-driver.get('http://localhost:3000/') 
+driver.get('https://app.dbtune.ai/') 
 
 EMAIL = driver.find_element(By.ID, "email")
-EMAIL.send_keys('aqsa123@mailinator.com')
+EMAIL.send_keys('mumair5393@gmail.com')
 
 PASSWORD = driver.find_element(By.ID, "password")
-PASSWORD.send_keys('Haierg30_')
+PASSWORD.send_keys('51478gameon')
 
 PASSWORD.send_keys(Keys.RETURN)
+time.sleep(10)
+driver.get('https://app.dbtune.ai/start-tuning-database')
 
 try:
+    time.sleep(5)
     element = WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.ID, "db_btn_Continue"))
     )
@@ -36,12 +40,6 @@ try:
     print(KEYS)
     COMMAND_SWCLIENT = "python3 __main__.py " + KEYS
     print(COMMAND_SWCLIENT)
-
-    # EC.presence_of_element_located(By.ID, "second-click")
-    # getCurlCopied = WebDriverWait(driver, 30).until(
-    #     EC.presence_of_element_located((By.ID, "second-click"))
-    # )
-    # getCurlCopied.click()
 
 except:
     driver.quit()
